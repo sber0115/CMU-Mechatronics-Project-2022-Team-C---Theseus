4f1a4e7d5c9d1ffe7e39f2e6908f7151d91f6d85
Valmiki Kothare <valmiki.kothare.vk@gmail.com>
2022-02-20 18:39:32 -0500

    GUI update

--- Motor Lab/motor_lab_gui_arduino/motor_lab_gui_arduino.ino
+++ Motor Lab/motor_lab_gui_arduino/motor_lab_gui_arduino.ino
@@ -2,31 +2,41 @@
 
 //servo
 Servo myservo;
-const uint16_t POT_PIN = A2;
+const uint16_t SERVO_PIN = 9;
 ////
 
+//potentiometer
+const uint16_t POT_PIN = A2;
+uint16_t pot_deg = 0;
+////
+
+//ping sensor
+const uint16_t PING_ECHO_PIN = 10;
+const uint16_t PING_TRIG_PIN = 11;
+const uint16_t SPEED_SOUND = 34;
+const uint16_t MAX_PING_DISTANCE = 60;
+uint32_t ping_distance = 0;
+////
+
+//force sensor
+uint32_t force_val = 0;
+////
 
 //stepper motor
 const uint16_t STEP_PIN = 8;
 const uint16_t STEPS_PER_REV = 200;
-uint32_t curr_step_angle = 0;
-uint32_t curr_step_count = 0;
+int curr_step_angle = 0;
+int curr_step_count = 0;
 ////
 
-//pin sensor
-const uint16_t PING_ECHO_PIN = 10;
-const uint16_t PING_TRIG_PIN = 11;
-const uint16_t SPEED_SOUND = 34;
-uint32_t ping_distance = 0;
-////
 
 int buf_len = 50;
 uint16_t deg;
 char pot_buf[50];
-
+char ping_buf[50];
+char force_buf[50];
 char incoming_buf[50];
 
-char ping_buf[50];
 size_t read_len;
 size_t write_len;
 char *token;
@@ -40,11 +50,10 @@ int servo_speed_sensor = 0;
 int dc_speed_sensor = 0;
 int stepper_angle_sensor = 0;
 
-uint16_t pot_deg = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  myservo.attach(9);
+  myservo.attach(SERVO_PIN);
   pinMode(PING_ECHO_PIN, INPUT);
   pinMode(PING_TRIG_PIN, OUTPUT);
   
@@ -65,9 +74,9 @@ void loop() {
       if(strcmp(token, "manual") == 0){
         token = strtok(NULL, ":");
         if(token != NULL){
-          if(strcmp(token, "true")){
+          if(strcmp(token, "true") == 0){
             manual = true;
-          }else if(strcmp(token, "false")){
+          }else if(strcmp(token, "false") == 0){
             manual = false;
           }
         }
@@ -94,7 +103,8 @@ void loop() {
 
   pot_deg = read_potentiometer();
   ping_distance = read_ping_sensor();
-  servo_speed_sensor = adjust_servo_speed(map(pot_deg, 0, 180, -50, 50));
+  servo_speed_sensor = map(pot_deg, 0, 180, -50, 50);
+  stepper_angle_sensor = map(ping_distance, 0, MAX_PING_DISTANCE, 0, 360);
   
   if(manual){
     adjust_servo_speed(servo_speed_manual);
@@ -103,14 +113,13 @@ void loop() {
   }else{
     adjust_servo_speed(servo_speed_sensor);
 //    adjust_dc_speed(dc_speed_manual);
-//    adjust_stepper_angle(stepper_angle_manual);
+    adjust_stepper_angle(stepper_angle_manual);
   }
   
-  //write_len = sprintf(pot_buf, "potentiometer:%d\n", pot_deg);
-  //Serial.write(pot_buf, write_len);
-  //delay(200);
+  write_len = sprintf(pot_buf, "potentiometer:%d\n", pot_deg);
+  Serial.write(pot_buf, write_len);
   
-  write_len = sprintf(ping_buf,"ping_sensor:%d\n", ping_distance);
+  write_len = sprintf(ping_buf,"ultrasonic:%d\n", ping_distance);
   Serial.write(ping_buf, write_len);
   delay(200);
   
@@ -140,24 +149,30 @@ uint32_t read_ping_sensor() {
 }
 
 
-int adjust_servo_speed(int servo_speed) {
+void adjust_servo_speed(int servo_speed) {
   myservo.writeMicroseconds(map(servo_speed,-50 ,50, 1309, 1710));
 }
 
 void adjust_stepper_angle(int target_angle){
-  curr_step_angle = map(curr_step_count,0,200,0,360);
+  curr_step_angle = map(curr_step_count,0,STEPS_PER_REV,0,360);
 
-  while (curr_step_angle != target_angle){
-    //Serial.println("Stepper Angle: " + String(curr_step_count) + " " + String(curr_step_angle));
-
-    digitalWrite(STEP_PIN, HIGH);
-    curr_step_count++;
+  while (abs(curr_step_angle - target_angle) > 5){
+    if ((curr_step_angle > target_angle && abs(curr_step_angle - target_angle) < 180) ||
+        (curr_step_angle < target_angle && abs(curr_step_angle - target_angle) >= 180)){
+      digitalWrite(STEP_PIN, LOW);
+      delayMicroseconds(500);
+      digitalWrite(STEP_PIN, HIGH);
+      delayMicroseconds(500);
+      curr_step_count--;
+    }else{
+      digitalWrite(STEP_PIN, HIGH);
+      delayMicroseconds(500);
+      digitalWrite(STEP_PIN, LOW);
+      delayMicroseconds(500);
+      curr_step_count++;
+    }
     curr_step_count %= STEPS_PER_REV;
-
-    curr_step_angle = map(curr_step_count,0,200,0,360);
-    delayMicroseconds(500);
-    digitalWrite(STEP_PIN, LOW);
-    delayMicroseconds(500);
+    curr_step_angle = map(curr_step_count,0,STEPS_PER_REV,0,360);
   }
   return;
 }
