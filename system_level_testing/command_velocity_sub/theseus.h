f436b3ac3daaa60a74c30bed71c5c0bce02458de
Sebastian <sbernal@andrew.cmu.edu>
2022-04-20 01:42:59 -0400

    Added system level testing folder

---
+++ system_level_testing/command_velocity_sub/theseus.h
@@ -0,0 +1,48 @@
+#include "Arduino.h"
+
+#ifndef theseus_h
+#define theseus_h
+
+  struct motor_t {
+    uint8_t EN;
+    uint8_t IN1;
+    uint8_t IN2;
+    uint8_t ENCA;
+    uint8_t ENCB;
+  };
+
+  enum move_t {
+    FWD=0,
+    BACK=1,
+    LEFT=2,
+    RIGHT=3,
+    ROT_CW=4,
+    ROT_CCW=5,
+    STOP=6
+  };
+  
+  enum dir_t {
+    CW=1,
+    CCW=2,
+    BRAKE=0
+  };
+
+  enum msg_t {
+    TEST=0,
+    MOVE=1
+  };
+
+  struct msg_test {
+    uint8_t id;
+    uint8_t data[3];
+  };
+
+  struct msg_move_t {
+    uint8_t dir;
+    uint8_t x_vel;
+    uint8_t y_vel;
+    uint8_t r_vel;
+  };
+
+
+#endif
